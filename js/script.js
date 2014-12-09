d9526cd6d8f2fd65116e0447915bf6ddade38696
Simeon Ackerman <amseon@web.de>
2014-12-09 10:02:06 +0100

    Add sample positions

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ RDFGraphVis = function(data) {
 	this.model = new Array();
 	this.subClasses = new Object();	
 	this.printedClasses = new Object();
+	this.storedModel = new Object();
 
 	this.element = function(elm, x, y, label) {};
 	this.link = function(elm1, elm2) {};
@@ -47,9 +48,12 @@ RDFGraphVis.prototype.init = function() {
 RDFGraphVis.prototype.parse = function(){
 	var _this  = this;
 
-	jsonld.fromRDF(_this.nquads, {format: 'application/nquads'}, function(err, doc) {
-		console.log( "Model:  ", doc );
+	jsonld.fromRDF(_this.nquads, {format: 'application/nquads'}, function(err, doc) {		
 		_this.model = doc;
+
+		_this.merge();
+
+		console.log( "Model:  ", doc );
 
 		// walk model
 		$.each( _this.model, function(key, element) {
@@ -67,7 +71,53 @@ RDFGraphVis.prototype.parse = function(){
 }
 
 RDFGraphVis.prototype.merge = function(){
+	var _this = this;
 
+	_this.storedModel = new Object({
+		"http://catalogus-professorum.org/cpm/Person" : new Object({
+			"@joint" : new Array( 
+				new Object({  "x" : 100, "y" : 100 }) 
+			)
+		}),
+		"http://catalogus-professorum.org/cpm/Professor" : new Object({
+			"@joint" : new Array( 
+				new Object({  "x" : 100, "y" : 400 }) 
+			)
+		}),
+	});
+
+	console.log("Stored Model:", _this.storedModel);
+
+	$.each( _this.model, function(key, element) {
+
+		if ( _this.storedModel.hasOwnProperty( element["@id"] ) ) {
+			//console.log( element, " inGraphData gefunden");
+			_this.model[key]["@joint"] = _this.storedModel[element["@id"]]["@joint"];
+		}
+
+	});
+
+	//return true;
+	/*
+	var test = new Array();
+	test.push(new Object({
+			"@id"	: "http://catalogus-professorum.org/cpm/Person",
+			"@type"	: new Array( "http://www.w3.org/2002/07/owl#Class" ),
+			"http://www.w3.org/2002/07/owl#bla"	: new Array( "testbla" ),
+			//"http://rdfgraphvis.de/joint#x" : new Array("100"),
+			"@joint" : new Array( new Object({ "x" : 100, "y": 200 }) ),
+	}));
+
+	console.log("test array:", test);
+
+	jsonld.toRDF(test, {format: 'application/nquads'}, function(err, nquads) {
+		console.log("test n3:", nquads);
+
+		jsonld.fromRDF(nquads, {format: 'application/nquads'}, function(err, doc) {
+			console.log("test n3 to jsonld:", doc);
+		});
+	});
+	*/
 }
 
 // print the model as JointJS graph
@@ -89,11 +139,18 @@ RDFGraphVis.prototype.print = function(element){
 		label = element["http://www.w3.org/2000/01/rdf-schema#label"][0]["@value"];
 	}
 
+	var x = 0;
+	var y = 0;	
+
 	switch( element["@type"][0] ) {
 		// print a class
 		case "http://www.w3.org/2002/07/owl#Class":
 			console.log("Print Class : ", element);
-			var thisClass = _this.element(_this.erd.Entity, 0, 0, label);
+			if ( element.hasOwnProperty("@joint") ) {
+				x = element["@joint"][0]["x"];
+				y = element["@joint"][0]["y"];
+			}
+			var thisClass = _this.element(_this.erd.Entity, x, y, label);
 			_this.printedClasses[element["@id"]] = thisClass;
 
 			if ( element.hasOwnProperty("http://www.w3.org/2000/01/rdf-schema#subClassOf") ) {
@@ -107,18 +164,19 @@ RDFGraphVis.prototype.print = function(element){
 		// print a property
 		case "http://www.w3.org/2002/07/owl#DatatypeProperty":
 		case "http://www.w3.org/2002/07/owl#FunctionalProperty":
-		case "http://www.w3.org/2002/07/owl#ObjectProperty":
-			console.log("Print Property : ", element);
+		case "http://www.w3.org/2002/07/owl#ObjectProperty":			
 			if ( ! element.hasOwnProperty("http://www.w3.org/2000/01/rdf-schema#domain") ) {
 				console.log("No domain in: ", element);
 				return false;
 			}
-			var thisProperty = _this.element(_this.erd.Normal, 0, 0, label);
+			
 			$.each( element["http://www.w3.org/2000/01/rdf-schema#domain"], function(key, domain) {
 				if ( ! _this.printedClasses.hasOwnProperty(domain["@id"]) ) {
 					console.log("Domain-Class not yet printed...: ", element);
 					return false;
 				}
+				console.log("Print Property : ", element);
+				var thisProperty = _this.element(_this.erd.Normal, x, y, label);
 				_this.link(thisProperty, _this.printedClasses[domain["@id"]]);
 			});
 			break;
