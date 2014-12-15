78f532cf81c05d4f5fb2c2000f3a4bec7c77050b
Simeon Ackerman <amseon@web.de>
2014-12-15 13:47:40 +0100

    Implement fullscreen interface and endless drag&drop

--- js/script.js
+++ js/script.js
@@ -5,12 +5,23 @@ RDFGraphVis = function( settings ) {
 	if ( settings.id ) {
 		this.id = settings.id;
 	}
+
+	this.state = {
+
+	};
+
+	//this.thisGraph = null;
+	this.svg = null;
+	this.svgGraph = null;
+	//this.svgLinks = null;
+	//this.svgNodes = null;
+
 	this.model = []; // store json-ls model
 	this.graphModel = { "nodes" : [], "links" : [] }; // store nodes and links
 	this.storedModel = {}; // stored model with positions
 	this.classProperties = {};
 
-	this.vis = null;
+	//this.vis = null;
 	this.force = null;
 	this.zoom = true;
 	
@@ -22,27 +33,36 @@ RDFGraphVis = function( settings ) {
 RDFGraphVis.prototype.init = function() {
 	var _this = this;
 
+	var docEl = document.documentElement;
+	var bodyEl = document.getElementsByTagName('body')[0];
+
 	// init graph
-	var w = 1000;
-	var h = 600;
+	var w = window.innerWidth || docEl.clientWidth || bodyEl.clientWidth;
+	var h = window.innerHeight|| docEl.clientHeight|| bodyEl.clientHeight;	
 
 	// init root svg, add zoom/pan g
-	_this.vis = d3.select("#graph").append("svg:svg")
+	_this.svg = d3.select("#graph").append("svg:svg")
 		.attr("width", w)
-		.attr("height", h)
+		.attr("height", h);
+		/*
 		.attr("pointer-events", "all")
 		.append('svg:g')
 		.call(d3.behavior.zoom().on("zoom", redraw))
 		.append('svg:g');
-
+		*/
+	/*
 	// append content rect
 	_this.vis.append('svg:rect')
 		.attr('width', w)
 		.attr('height', h)
 		.attr('fill', 'rgba(1,1,1,0)');
+	*/
+
+	//console.log( parseInt(_this.svg.attr("width")) );
+	//return false;
 
 	// define the arrow.
-	_this.vis.append("svg:defs").selectAll("marker")
+	var defs = _this.svg.append("svg:defs").selectAll("marker")
 		.data(["end"])      // Different link/path types can be defined here
 		.enter().append("svg:marker")    // This section adds in the arrows
 		.attr("id", String)
@@ -63,10 +83,71 @@ RDFGraphVis.prototype.init = function() {
 		.linkDistance( 100 )
 		.size([w, h]);
 
+	/*
 	function redraw() {
 		if ( _this.zoom )
 			_this.vis.attr("transform","translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
 	}
+	*/
+
+	_this.svgGraph = _this.svg.append("g")
+		.attr("class", "this-graph");
+
+
+	//d3.select(".this-graph")
+      		//.attr("transform", "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")"); 
+
+// svg nodes and edges 
+    //_this.svgLinks = _this.svgGraph.append("g").selectAll("g");
+    //_this.svgNodes = _this.svgGraph.append("g").selectAll("g");		
+
+    _this.drag = d3.behavior.drag()
+          .origin(function(d){
+            return {x: d.x, y: d.y};
+          })
+          .on("drag", function(args){
+            _this.state.justDragged = true;
+            _this.dragmove.call(_this, args);
+          })
+          .on("dragend", function() {
+            // todo check if edge-mode is selected
+          });
+
+// listen for key events
+    d3.select(window).on("keydown", function(){
+      _this.svgKeyDown.call(_this);
+    })
+    .on("keyup", function(){
+      _this.svgKeyUp.call(_this);
+    });
+    _this.svg.on("mousedown", function(d){_this.svgMouseDown.call(_this, d);});
+    _this.svg.on("mouseup", function(d){_this.svgMouseUp.call(_this, d);});          
+
+	// listen for dragging
+    var dragSvg = d3.behavior.zoom()
+          .on("zoom", function(){
+            if (d3.event.sourceEvent.shiftKey){
+              // TODO  the internal d3 state is still changing
+              return false;
+            } else{
+              _this.zoomed.call(_this);
+            }
+            return true;
+          })
+          .on("zoomstart", function(){
+            //if (!d3.event.sourceEvent.shiftKey) d3.select('body').style("cursor", "move");
+          })
+          .on("zoomend", function(){
+            d3.select('body').style("cursor", "auto");
+          });
+
+    _this.svg.call(dragSvg).on("dblclick.zoom", null);        
+
+	//return false;
+	// listen for resize
+    window.onresize = function(){_this.updateWindow();};
+
+    //return false;
 
 	// get nquads to json
 	jsonld.fromRDF(_this.nquads, {format: 'application/nquads'}, function(err, doc) {		
@@ -77,11 +158,11 @@ RDFGraphVis.prototype.init = function() {
 
 		// try to get bas from ontology		
 		if ( _this.id == "" && _this.model[0]["@type"] == "http://www.w3.org/2002/07/owl#Ontology" ) {
-			_this.id = encodeURIComponent( _this.model[0]["@id"] );
+			_this.id = _this.model[0]["@id"];
 		}
 			
 		// maybe get stored graph
-		$.post( "ajax/get.php", { name: _this.id + ".json" })		
+		$.post( "ajax/get.php", { name: encodeURIComponent( _this.id ) + ".json" })		
 			.done(function( jsondata ) {
 				if ( jsondata.result && jsondata.content != "" ) {
 					_this.storedModel = $.parseJSON( jsondata.content );
@@ -93,11 +174,45 @@ RDFGraphVis.prototype.init = function() {
 
 } // end of init
 
+RDFGraphVis.prototype.zoomed = function(){
+    //this.state.justScaleTransGraph = true;
+    if ( this.zoom )
+    	d3.select(".this-graph")
+      		.attr("transform", "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")"); 
+  };
+
+ RDFGraphVis.prototype.updateWindow = function(){
+ 	var _this = this;
+    var docEl = document.documentElement,
+        bodyEl = document.getElementsByTagName('body')[0];
+    var x = window.innerWidth || docEl.clientWidth || bodyEl.clientWidth;
+    var y = window.innerHeight|| docEl.clientHeight|| bodyEl.clientHeight;
+    _this.svg.attr("width", x).attr("height", y);
+  };
+
+// mousedown on main svg
+  RDFGraphVis.prototype.svgMouseDown = function(){
+    this.state.graphMouseDown = true;
+  };  
+
+ // mouseup on main svg
+  RDFGraphVis.prototype.svgMouseUp = function(){
+    this.state.graphMouseDown = false;
+  };  
+
+  RDFGraphVis.prototype.svgKeyDown = function(){
+    
+  };
+
+  RDFGraphVis.prototype.svgKeyUp = function(){
+    
+  };  
+
 // parse the rdf to json-ld
 RDFGraphVis.prototype.parse = function(){
 	var _this  = this;
 
-	$("#graph-id").val( encodeURIComponent( _this.id ) );
+	$("#graph-id").val(  _this.id );
 
 
 	var nodeIndexes = new Object();
@@ -274,6 +389,7 @@ RDFGraphVis.prototype.parse = function(){
 RDFGraphVis.prototype.print = function(){
 	var _this = this;
 
+
 	// node drag by funktion
 	// http://bl.ocks.org/norrs/2883411	
 	var node_drag = d3.behavior.drag()
@@ -282,7 +398,7 @@ RDFGraphVis.prototype.print = function(){
 	    .on("dragend", dragend);	
 
 	// add links
-	var link = _this.vis.selectAll(".link")
+	var link = _this.svgGraph.selectAll(".link")
 		.data(_this.graphModel.links)
 		.enter().append("line")
 		.attr("class", function(d) {
@@ -297,7 +413,7 @@ RDFGraphVis.prototype.print = function(){
 		.style("stroke", "gray");    
 		
 	// add nodes
-	var node = _this.vis.selectAll(".node")
+	var node = _this.svgGraph.selectAll(".node")
 		.data(_this.graphModel.nodes)
 		.enter().append("svg:g")
 		//.attr("class", "node")
@@ -372,12 +488,41 @@ RDFGraphVis.prototype.print = function(){
 	node.append("title")
 		.text(function(d) { return d["@id"] } );
 
+	var minX = 0;
+	var maxX = 0;
+	var minY = 0;
+	var maxY = 0;
+
 	// set nodes fixed position if they already have x and y
-	_this.vis.selectAll("g.node").filter(function(d){
+	_this.svgGraph.selectAll("g.node").filter(function(d){		
+		if ( d.x > maxX ) maxX = d.x;
+		if ( d.x < minX ) minX = d.x;
+		if ( d.y > maxY ) maxY = d.y;
+		if ( d.y < minY ) minY = d.y;
 		if ( d.hasOwnProperty("x") && d.hasOwnProperty("y") ) {
 			d.fixed = true;
 			return true;
 		}})
+
+	/*
+	// TODO: initial zoom/translation
+	if ( Object.keys(_this.storedModel).length > 0 ) {
+		var translX = (parseInt(_this.svg.attr("width")) - ( maxX - minX )) / 2;
+		var translY = (parseInt(_this.svg.attr("height")) - ( maxY - minY )) / 2;
+		//console.log( d3.event.translate );		
+		d3.event = { "type" : "zoom", "scale" : 1, "translate": [translX, translY] };
+		//d3.select(".this-graph")
+		//	.attr("transform", "translate("+translX+","+translY+") scale(1)"); 
+		
+		//set the min and max extent to which zooming can occur and define a mouse zoom function
+		//var zoom = d3.behavior.zoom().scaleExtent([0.3, 3]).on("zoom", _this.zoomed);
+		//zoom.translate([500,50]).scale(2);//translate and scale to whatever value you wish
+		//zoom.event(_this.svgGraph.transition().duration(50));//does a zoom
+		_this.zoomed();
+	}
+	*/
+
+	
 
 	// auto width class and property-boxes
 	arr1 = d3.selectAll("text.label");
@@ -438,7 +583,7 @@ RDFGraphVis.prototype.print = function(){
 	    	// move the properties of this class
 	    	$.each( _this.classProperties[d["@id"]], function() {
 	    		this.px += d3.event.dx;
-			    this.py += d3.event.dy;
+	    		this.py += d3.event.dy;
 			    this.x += d3.event.dx;
 			    this.y += d3.event.dy; 
 	    	});
@@ -456,10 +601,8 @@ RDFGraphVis.prototype.print = function(){
 	        .attr("y1", function(d) { return d.source.y; })
 	        .attr("x2", function(d) { return d.target.x; })
 	        .attr("y2", function(d) { return d.target.y; });
-
 	    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });    
 	}
-
 }
 
 // update the graph
@@ -471,8 +614,8 @@ RDFGraphVis.prototype.update = function() {
 	_this.graphModel.nodes = [];
 	_this.graphModel.links = [];
 
-	_this.vis.selectAll("g.node").remove();
-	_this.vis.selectAll("line").remove();
+	_this.svg.selectAll("g.node").remove();
+	_this.svg.selectAll("line").remove();
 	
 	_this.parse();
 }
@@ -507,7 +650,7 @@ RDFGraphVis.prototype.save = function() {
 
 	content = JSON.stringify( content );
 
-	$.post( "ajax/save.php", { name: graphId + ".json", content: content })
+	$.post( "ajax/save.php", { name: encodeURIComponent( graphId ) + ".json", content: content })
 		.done(function( jsondata ) {
 			if ( jsondata.result ) {
 				//$(resultMsg).addClass("text-success");
@@ -523,9 +666,9 @@ RDFGraphVis.prototype.save = function() {
 RDFGraphVis.prototype.toggleProperties = function(hide) {
 	var _this = this;
 	var display = hide ? "none" : "inline";
-	_this.vis.selectAll(".property-node")
+	_this.svgGraph.selectAll(".property-node")
 		.style("display", display);
 
-	_this.vis.selectAll(".property-link")
+	_this.svgGraph.selectAll(".property-link")
 		.style("display", display);
 }
